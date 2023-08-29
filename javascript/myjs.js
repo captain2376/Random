10bf0131aaa45f3e4132cbe27d6d3d9152311b71
captain2376 <b17296@rgukt.ac.in>
2023-08-29 12:54:43 +0530

    29/08/2023

---
+++ javascript/myjs.js
@@ -0,0 +1,53 @@
+//document.write(prompt("Enter username"))
+// let username=prompt("enter name")
+// document.write(username)
+//document.write(confirm("Would you like to exit?"))
+document.getElementById("my-content").innerHTML="This is dynamic content"
+// var a=90
+// var b=9
+// document.write(a+b,"<br>")
+// document.write(typeof a,"<br>")
+// let i=22
+// document.write(typeof a)
+
+    //String
+// let s="Sindhu"
+// document.write(s.length,"<br>")
+// document.write(s.toUpperCase(),"<br>")
+// document.write(s.toLowerCase(),"<br>")
+
+
+//boolean
+// var a=true
+// var b=false
+// document.write(typeof a)
+
+// var ob={'username':'Sindhuja'}
+// document.write(typeof ob,"<br>")
+// document.write(ob['username'])
+
+//array
+// var nums=[1,2,3,4,5]
+// document.write(typeof nums,"<br>")//it is a type of object
+// document.write(nums[3],"<br>")
+// document.write(nums.length,"<br>")
+
+    //CONTROL STATEMENTS
+// var n=prompt("enter a number")
+// var a=parseInt(n)
+// if(a>0)
+// {
+//     document.write("positive")
+// }
+// else if(a<0)
+// {
+//     document.write("negative")
+// }
+// else{
+//     document.write("zero")
+// }
+
+    //FOR LOOP
+for (let i = 0; i <=10; i++) {
+    document.write(i,"<br>")
+}
